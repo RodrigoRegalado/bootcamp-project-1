13928c7b17c6ee7276f571f225d095969a7c72a2
Rodrigo Regalado <r.regalado6204@gmail.com>
2022-11-02 21:29:28 -0700

    fixed div layout

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function createRandomString () {
     return Math.floor(Math.random()*5001) + Math.random().toString(36).substring(2,4)
 }
 
+// let savednumbers = document.getElementsByClassName(setnumber)
+document.getElementById("setnumber1").innerhtml= createRandomString();
+
+console.log(document.getElementById("setnumber1").innerhtml= createRandomString())
 console.log(createRandomString())
